f7cff4b3b76ecd82c07b47a89e64a8ed3ad8d643
sushpicious <2011658@apps.nsd.org>
2023-02-15 00:32:14 -0800

    Working build

--- src/App.js
+++ src/App.js
@@ -63,9 +63,23 @@ const App=()=> {
     }
   ]);
 
+  const [currentSongIndex,setCurrentSongIndex] = useState(0);
+const [nextSongIndex,setNextSongIndex] = useState(currentSongIndex + 1);
+
+
+useEffect(()=>{
+  setNextSongIndex(()=>{
+  if (currentSongIndex + 1 >songs.length - 1 ){
+    return 0;
+  } else{
+    return currentSongIndex + 1;
+  }
+});
+},[currentSongIndex])
+
   return (
     <div className="App">
-      MusicPlayer
+    <Player currentSongIndex={currentSongIndex} setCurrentSongIndex={setCurrentSongIndex} nextSongIndex={nextSongIndex} songs={songs} />
     </div>
   );
 }
